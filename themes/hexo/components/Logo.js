4ba7d0cea798b277f809702f71aac7d6f1562324
zhile55555 <zhile55555@gmail.com>
2025-06-22 19:20:35 +0800

    Update Logo.js

--- themes/hexo/components/Logo.js
+++ themes/hexo/components/Logo.js
@@ -10,12 +10,12 @@ const Logo = props => {
   const { siteInfo } = props
   return (
     <Link href='/' passHref legacyBehavior>
-   //  <div className='flex flex-col justify-center items-center cursor-pointer space-y-3'>
+    // <div className='flex flex-col justify-center items-center cursor-pointer space-y-3'>
         <div className='font-medium text-lg p-1.5 rounded dark:border-white dark:text-white menu-link transform duration-200'>
           {' '}
           {siteInfo?.title || siteConfig('TITLE')}
         </div>
-      </div>
+   //   </div>
     </Link>
   )
 }
